2f2108fc086557e00082ab8e106adde17fbeabef
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2022-04-04 19:52:31 +0530

    post api

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,4 +1,67 @@
+const validUrl = require('valid-url')
+const shortid = require('short-id')
+
+const urlModel = require('../model/UrlModel')
+
+const isValid = (value) => {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+}
+
+const generateShortUrl = async (req, res) => {
+
+    try {
+
+        const { longUrl } = req.body
+
+        if (!isValid(longUrl)) return res.status(400).send({ error: "Please enter Url" })
+
+        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, message: "Url is not valid" })
+
+        const baseUrl = 'http:localhost:3000'
+
+        if (!validUrl.isUri(baseUrl)) return res.status(400).send({ status: false, message: "baseUrl is not valid" })
+
+        const urlCode = shortid.generate()
+
+        const shortUrl = baseUrl + '/' + urlCode
+
+        const newUrl = {
+            longUrl: longUrl,
+            shortUrl: shortUrl,
+            urlCode: urlCode
+        }
+
+        const generatedUrl = await urlModel.create(newUrl)
+        return res.status(201).send({ status: true, data: newUrl })
+    }
+    catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
+}
+
+//-----------------------------------------------------------------------------------------------------
 
 
+const getUrl = async (req, res) => {
+    try {
+        const { urlCode } = req.params;
+
+        if (!isValid(urlCode)) return res.status(400).send({ status: false, message: "Invalid Url" })
+
+        const result = await urlModel.findOne({urlCode })
+        if (!result) {
+            return res.status(404).send({ status: false, msg: "Url doesn't exist" });
+        }
+
+        res.status(301).redirect(result.longUrl)
 
 
+    } catch (error) {
+        res.status(500).send({ status: true, msg: error.message })
+    }
+}
+
+module.exports.generateShortUrl = generateShortUrl
+module.exports.getUrl = getUrl
--- src/route/route.js
+++ src/route/route.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const router = express.Router()
+const UrlController = require("../controller/urlController")
+
+router.post("/url/shorten",UrlController.generateShortUrl)
+
+router.get("/:urlCode",UrlController.getUrl)
 
 
 module.exports = router;
